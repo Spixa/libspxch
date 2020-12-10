82deba31146b1b4512ae0aacf867eedaefb41e0a
Spixa <74703416+Spixa@users.noreply.github.com>
2020-12-10 12:16:55 +0330

    Added source files
    Main.cpp is an example file. It isn't needed.

---
+++ src/libspxch.h
@@ -0,0 +1,92 @@
+/*
+      Command Line Handler - Distributable and Library version
+
+    Single file
+         Coded by: Spixa
+  Sourcecode repository for GitHub on Spixa
+  For education purposes only.
+
+
+
+*/
+// THIS CODE IS "NOT" C FRIENDLY
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+// Command Handle Class
+class CommandHandle {
+public:
+
+        CommandHandle(std::string load) {
+                std::ifstream file;
+                std::string buff;
+		
+		file.open(load);
+		if (!(file.is_open())) {
+			if (load == "") load = "a non-existing file";
+			std::cout << "I couldn't open command data.\n\tCouldn't open " << load << '\n';
+			return;
+		}
+                while (file >> buff) {
+                        commands.push_back(buff);
+
+
+                }
+
+
+                for (int i = 0;i < commands.size();i++ ) {
+                 std::cout << "Loaded command: " << commands[i] << std::endl;
+               }
+                file.close();
+        }
+//      const static unsigned int m_args = 256;
+    std::vector<std::string> commands;
+
+
+    std::string access(std::string str,int accint) {
+            std::string args[256];
+            process(str,args);
+
+        for (int i = 0; i < commands.size();i++) {
+                if (args[0]  == commands[i]) {
+                     return args[accint];
+                }
+
+        }
+        return "$unhandled";
+
+    }
+        std::string getLastBuffer() {return last_buffer;} // Gets last buffer.
+
+private:
+     void process(std::string str, std::string  arg[])
+        {
+                std::string word = "";
+                int i = 0;
+                for (auto x : str)
+                {
+                        if (x == ' ')
+                        {
+                                i++;
+                                arg[i] = word;
+
+                                word = "";
+                        }
+                        else
+                        {
+                                word = word += x;
+                        }
+
+                        arg[i] = word;
+                }
+        }
+
+
+
+
+
+
+        std::string last_buffer;
+        // Wiki & Doc error: last_buffer is within a private context. Use the getter function
+};
---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include "libspxch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+        string t;
+
+        string command;
+        string token1;
+        string token2;
+
+
+        CommandHandle c("file.cmdlist");
+        cout << "\n\n";
+        while(true) {
+                cout << "Open Command: ";
+                getline(cin,t);
+
+                command = c.access(t,0);
+                token1 = c.access(t,1);
+                token2 = c.access(t,2);
+
+                for (int i = 0;i < c.commands.size();i++) {
+                        if (c.commands[i] == command) {
+                                std::cout << "Command index: " << i << endl;
+                                break;
+                        }
+
+                }
+
+                if (command == "do") {
+                        if (token1 == "say") {
+                                cout << token2 << endl; // Prints the second argument of "do say [text]". Add "do" to file.cmdlist to make this work.
+                        }
+                } else
+
+                if (command == "$unhandled") {
+                        cout << "Command wasn't found\n\n";
+                }
+        }
+}
